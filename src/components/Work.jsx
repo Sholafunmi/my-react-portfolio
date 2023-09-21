5d835a50d385ccede0686dd193f4721cb12b2a6b
Sholafunmi <sholafunmiadewara@gmail.com>
2023-09-21 05:43:12 +0100

    spillafricaURL

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -57,7 +57,7 @@ const Work = () => {
                             HTML/ CSS/ JAVASCRIPT
                         </span>
                         <div className='pt-8 text-center'>
-                            <a href="https://spillafrica.vercel.app/" target='_blank '>
+                            <a href="https://spillafrica.com/" target='_blank '>
                                 <button className='text-center rounded-lg px-4 py-4 m-2 bg-white  text-gray-700 font-bold text-lg'>
                                     View Project
                                 </button>
